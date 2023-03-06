8895e533609265d710884c27588a4a0a4eca4653
prudhvi1601 <prudhvi1601@gmail.com>
2023-03-06 16:13:30 +0530

    listToArray

---
+++ List to Array/script.js
@@ -0,0 +1,14 @@
+ const dat = {"name" : "raj", "age" : 24};
+
+//  const arr = [];
+// for(let key in list){
+//     arr.push(key,list[key]);
+// }
+// console.log(arr);
+  
+console.log(Object.entries(dat));
+console.log(Object.keys(dat));
+console.log(Object.values(dat));
+
+
+       
