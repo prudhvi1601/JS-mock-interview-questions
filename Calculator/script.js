552e75b7dea943d66b6e88d4228349e880801374
prudhvi1601 <prudhvi1601@gmail.com>
2023-03-07 11:59:55 +0530

    calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,13 +1,32 @@
-const num1 = document.querySelector('#number1');
-const num2 = document.querySelector('#number2');
-const operator = document.querySelector('#operator');
-const button = document.querySelectorAll('#operator input');
-const result = document.querySelector('#result')
+let num1 = document.querySelector('#number1');
+let num2 = document.querySelector('#number2');
+let result = document.querySelector('#result');
+let a = document. querySelector('.car');
+let b = document.querySelector('.bike');
+let c = document.querySelector('.pen');
+let d = document.querySelector('.ink');
 
- const str = num1.innerHTML + button.innerText + num2.innerHTML;
- function evaluate(){
-    result.innerHTML = eval(str);
- }
+function addition(){
 
+   result.value = Number(num1.value) + Number(num2.value);
+}
 
-button.addEventListener('click',evaluate);
+function subtraction(){
+
+   result.value = Number(num1.value) - Number(num2.value);
+}
+
+function product(){
+
+   result.value = Number(num1.value) * Number(num2.value);
+}
+
+function division(){
+
+   result.value = Number(num1.value) / Number(num2.value);
+}
+
+a.addEventListener("click",addition);
+b.addEventListener("click",subtraction);
+c.addEventListener("click",product);
+d.addEventListener("click",division);
